6c32be0130051b1d0fd21c5540c3882ee8b0d633
Felipe Augusto <sonaldo@me.com>
2023-01-17 22:37:48 -0300

    build

---
+++ build/Release/.deps/Release/obj.target/node_gd/src/addon.o.d
@@ -0,0 +1,28 @@
+cmd_Release/obj.target/node_gd/src/addon.o := c++ -o Release/obj.target/node_gd/src/addon.o ../src/addon.cc '-DNODE_GYP_MODULE_NAME=node_gd' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DNAPI_CPP_EXCEPTIONS' '-DHAVE_LIBAVIF' '-DGD_AVIF' '-DHAVE_LIBTIFF' '-DGD_TIFF' '-DHAVE_LIBJPEG' '-DGD_JPEG' '-DHAVE_LIBFONTCONFIG' '-DGD_FONTCONFIG' '-DHAVE_LIBFREETYPE' '-DGD_FREETYPE' '-DHAVE_LIBPNG' '-DGD_PNG' '-DHAVE_LIBWEBP' '-DGD_WEBP' '-DBUILDING_NODE_EXTENSION' -I/Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/include/node -I/Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/src -I/Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/deps/openssl/config -I/Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/deps/openssl/openssl/include -I/Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/deps/uv/include -I/Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/deps/zlib -I/Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/deps/v8/include -I/Users/felipeaugusto/Documents/GitHub/tibia-corp/node-gd/node_modules/node-addon-api -I/opt/homebrew/include -I/usr/local/include -I/opt/local/include  -O3 -gdwarf-2 -fvisibility=hidden -mmacosx-version-min=10.13 -arch arm64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++14 -stdlib=libc++ -fno-rtti -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/node_gd/src/addon.o.d.raw   -c
+Release/obj.target/node_gd/src/addon.o: ../src/addon.cc \
+  /Users/felipeaugusto/Documents/GitHub/tibia-corp/node-gd/node_modules/node-addon-api/napi.h \
+  /Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/include/node/node_api.h \
+  /Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/include/node/js_native_api.h \
+  /Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/include/node/js_native_api_types.h \
+  /Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/include/node/node_api_types.h \
+  /Users/felipeaugusto/Documents/GitHub/tibia-corp/node-gd/node_modules/node-addon-api/napi-inl.h \
+  /Users/felipeaugusto/Documents/GitHub/tibia-corp/node-gd/node_modules/node-addon-api/napi-inl.deprecated.h \
+  ../src/node_gd.h /opt/homebrew/include/gd.h \
+  /opt/homebrew/include/gd_io.h /opt/homebrew/include/gdfx.h \
+  ../src/node_gd.cc ../src/node_gd_workers.cc \
+  /opt/homebrew/include/gd_errors.h
+../src/addon.cc:
+/Users/felipeaugusto/Documents/GitHub/tibia-corp/node-gd/node_modules/node-addon-api/napi.h:
+/Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/include/node/node_api.h:
+/Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/include/node/js_native_api.h:
+/Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/include/node/js_native_api_types.h:
+/Users/felipeaugusto/Library/Caches/node-gyp/16.13.0/include/node/node_api_types.h:
+/Users/felipeaugusto/Documents/GitHub/tibia-corp/node-gd/node_modules/node-addon-api/napi-inl.h:
+/Users/felipeaugusto/Documents/GitHub/tibia-corp/node-gd/node_modules/node-addon-api/napi-inl.deprecated.h:
+../src/node_gd.h:
+/opt/homebrew/include/gd.h:
+/opt/homebrew/include/gd_io.h:
+/opt/homebrew/include/gdfx.h:
+../src/node_gd.cc:
+../src/node_gd_workers.cc:
+/opt/homebrew/include/gd_errors.h:
