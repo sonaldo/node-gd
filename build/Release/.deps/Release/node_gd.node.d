4f101d98aee15c44a5389beb3c0fe5fd46766dbf
Felipe Augusto <sonaldo@me.com>
2023-01-17 23:50:05 -0300

    remove build

--- build/Release/.deps/Release/node_gd.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/node_gd.node := c++ -bundle -undefined dynamic_lookup -Wl,-search_paths_first -mmacosx-version-min=10.13 -arch arm64 -L./Release -stdlib=libc++  -o Release/node_gd.node Release/obj.target/node_gd/src/addon.o Release/nothing.a -lgd -L/opt/homebrew/lib -L/usr/local/lib -L/opt/local/lib
